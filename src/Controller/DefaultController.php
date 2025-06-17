83fca0a50c59e1aef646964ff5e9ee2ea4849a55
Tomasz Gradowski <to.gradowski@gmail.com>
2025-06-17 13:56:46 +0200

    uwkzdc.php

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -10,11 +10,11 @@ use Symfony\Bundle\SecurityBundle\Security;
 
 class DefaultController extends AbstractController
 {
-    #[Route('/', name: 'app_root_landing')] // Zmieniono nazwę trasy, aby była unikalna
-    public function index(Security $security): Response // Dodano wstrzyknięcie Security
+    #[Route('/', name: 'app_root_landing')]
+    public function index(Security $security): Response
     {
         if ($security->isGranted('IS_AUTHENTICATED_FULLY')) {
-            return $this->redirectToRoute('app_home'); // Przekierowanie na nową trasę /home
+            return $this->redirectToRoute('app_home');
         }
 
         return $this->redirectToRoute('app_register');
