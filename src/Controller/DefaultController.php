1a937ed8e8b4c2c6e7942dcb14754b9e0513c9e9
Tomasz Gradowski <to.gradowski@gmail.com>
2025-06-17 13:52:55 +0200

    taa

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\Security\Core\Security; // Ważne: dodaj tę linię
+use Symfony\Bundle\SecurityBundle\Security;
 
 class DefaultController extends AbstractController
 {
